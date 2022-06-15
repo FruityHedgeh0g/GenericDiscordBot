920bffe78e7bc92accb44a64d98eb90da3db27a3
FruityHedgeh0g <julien.jjd.peynot@gmail.com>
2022-06-16 01:47:20 +0200

    Initial

---
+++ src/main/java/fr/hedgehog/genericdiscordbot/GenericDiscordBotApplication.java
@@ -0,0 +1,13 @@
+package fr.hedgehog.genericdiscordbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenericDiscordBotApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GenericDiscordBotApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/hedgehog/genericdiscordbot/GenericDiscordBotApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.hedgehog.genericdiscordbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenericDiscordBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
